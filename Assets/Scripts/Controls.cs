f7cfbdf0c2b7fd9770484580797b90c9b88a26fe
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2024-03-18 21:35:59 -0400

    Vertical Movement Done
    The movement for the top half is done. Jumping still needs work with the gravity and the controls for the walls needs to be done better

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -6,8 +6,9 @@ public class Controls : MonoBehaviour
 {
     public float speed;
     public Rigidbody rb;
-    public float jumpAmount = 5;
+    public float jumpAmount = 2.5f;
     public bool jumping = false;
+    public int multi = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,36 +19,66 @@ public class Controls : MonoBehaviour
     void Update()
     {
         
-        Vector3 Movement = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        Vector3 Movement = new Vector3(multi*Input.GetAxis("Horizontal"), 0, 0);
         transform.position += Movement * speed * Time.deltaTime;
         //starting rotation
-        if (transform.position.x > -4 && transform.position.x <5)
+        if (transform.position.y > 15.1)
         {
-           
+            //Movement = Vector3(-Input.GetAxis("Horizontal"), 0, 0);
+            multi = -1;
+        } else
+            //Movement = Vector3(Input.GetAxis("Horizontal"), 0, 0);
+        multi = 1;
+        /*if (transform.position.x > 10 && Input.GetKey(KeyCode.A))//rightwall
+        {
+            rb.velocity = transform.up * speed;
         }
-        //left platform from starting
-        if (transform.position.x > -10 && transform.position.x < -5)
+        if (transform.position.x > 10 && Input.GetKey(KeyCode.D))//rightwall
         {
+            rb.velocity = -transform.up * speed;
+        }
+        if (transform.position.x < 10 && Input.GetKey(KeyCode.D))//leftwall
+        {
+            rb.velocity = transform.up * speed;
+        }
+        if (transform.position.x < 10 && Input.GetKey(KeyCode.A))//leftwall
+        {
+            rb.velocity = -transform.up * speed;
+        }*/
 
-        }
+
+
         jump();
     }
 
     void jump()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && jumping==false)
+        if (Input.GetKeyDown(KeyCode.Space) && jumping==false && transform.position.y < 10)
         {
-            StartCoroutine(SingleJump());
+            StartCoroutine(SingleJumpUp());
             
         }
-        
+        if (Input.GetKeyDown(KeyCode.Space) && jumping == false && transform.position.x < -4.5)
+        {
+            StartCoroutine(SingleJumpRight());
+
+        }
+
     }
 
-    IEnumerator SingleJump()
+    IEnumerator SingleJumpUp()
     {
         
         jumping = true;
         rb.AddForce(Vector3.up * jumpAmount, ForceMode.Impulse);
+        yield return new WaitForSeconds(2.0f);
+        jumping = false;
+    }
+
+    IEnumerator SingleJumpRight()
+    {
+        jumping = true;
+        rb.AddForce(Vector3.right * jumpAmount, ForceMode.Impulse);
         yield return new WaitForSeconds(2.0f);
         jumping = false;
     }
