43513214b9651bc44adb9de441065decd60fbfac
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2024-03-27 14:42:24 -0400

    Update CameraController.cs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,6 +6,15 @@ public class CameraController : MonoBehaviour
 {
     private GameObject player;
     private bool alive;
+    private Vector3 endPosition = new Vector3(5, -2, 0);
+    private Vector3 startPosition;
+    private float desiredDuration = 3f;
+    private float elapsedTime;
+    public Transform target;
+    public float movementTime;
+    public float rotationSpeed;
+    Vector3 refPos;
+    Vector3 refRot;
 
     // Start is called before the first frame update
 
@@ -21,17 +30,22 @@ public class CameraController : MonoBehaviour
         if (alive)
         {
 
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / desiredDuration;
 
+            
             switch (player.GetComponent<Controls>().getGarVNum())//rotate player and camera
             {
-                case 1: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0); player.transform.rotation = Quaternion.Euler(0, 0, 0); break;
-                case 2: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 45); player.transform.rotation = Quaternion.Euler(0, 0, 45); break;
-                case 3: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 90); player.transform.rotation = Quaternion.Euler(0, 0, 90); break;
-                case 4: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 135); player.transform.rotation = Quaternion.Euler(0, 0, 135); break;
-                case 5: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 180); player.transform.rotation = Quaternion.Euler(0, 0, 180); break;
-                case 6: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 225); player.transform.rotation = Quaternion.Euler(0, 0, 225); break;
-                case 7: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 270); player.transform.rotation = Quaternion.Euler(0, 0, 270); break;
-                case 8: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 315); player.transform.rotation = Quaternion.Euler(0, 0, 315); break;
+                case 1: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0); player.transform.rotation = Quaternion.Euler(0, 0, 0); 
+                        //transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete); startPosition = endPosition; 
+                        break;//Floor
+                case 2: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 45), rotationSpeed * Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 45); break;//RightFloor
+                case 3: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 90), rotationSpeed * Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 90); break;//RightWall
+                case 4: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 135), rotationSpeed * Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 135); break;//RightRoof
+                case 5: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 180), rotationSpeed* Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 180); break;//Roof
+                case 6: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 225), rotationSpeed* Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 225); break;//LeftRoof
+                case 7: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 270), rotationSpeed* Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 270); break;//LeftWall
+                case 8: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 315), rotationSpeed* Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 315); break;//LeftFloor
             }
         }
        
