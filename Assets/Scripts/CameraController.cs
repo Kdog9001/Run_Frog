abea4c784394c8ca0be1dce85ffc0f7415bc2df8
Kdog9001 <146856471+Kdog9001@users.noreply.github.com>
2024-03-27 14:48:37 -0400

    Camera Controller Slerp final

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,9 @@ public class CameraController : MonoBehaviour
 {
     private GameObject player;
     private bool alive;
-    private Vector3 endPosition = new Vector3(5, -2, 0);
-    private Vector3 startPosition;
-    private float desiredDuration = 3f;
-    private float elapsedTime;
-    public Transform target;
-    public float movementTime;
     public float rotationSpeed;
-    Vector3 refPos;
-    Vector3 refRot;
+    //Vector3 refPos;
+    //Vector3 refRot;
 
     // Start is called before the first frame update
 
@@ -30,14 +24,13 @@ public class CameraController : MonoBehaviour
         if (alive)
         {
 
-            elapsedTime += Time.deltaTime;
-            float percentageComplete = elapsedTime / desiredDuration;
+            
 
             
             switch (player.GetComponent<Controls>().getGarVNum())//rotate player and camera
             {
-                case 1: Camera.main.transform.rotation = Quaternion.Euler(0, 0, 0); player.transform.rotation = Quaternion.Euler(0, 0, 0); 
-                        //transform.position = Vector3.Lerp(startPosition, endPosition, percentageComplete); startPosition = endPosition; 
+                case 1: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 0), rotationSpeed * Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 0); 
+                        
                         break;//Floor
                 case 2: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 45), rotationSpeed * Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 45); break;//RightFloor
                 case 3: Camera.main.transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, 0, 90), rotationSpeed * Time.deltaTime); player.transform.rotation = Quaternion.Euler(0, 0, 90); break;//RightWall
