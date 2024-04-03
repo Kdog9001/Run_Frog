d27d0d350666699b2358bc4f776059bcfce687dd
AndrewOL1 <aoleary7@student.fitchburgstate.edu>
2024-04-03 14:18:26 -0400

    playerAudioFix

--- Assets/Scripts/Controls.cs
+++ Assets/Scripts/Controls.cs
@@ -26,6 +26,7 @@ public class Controls : MonoBehaviour
         curPos = 1;
         StartCoroutine(startTimer());
         setJumpAmount = jumpAmount;
+        soundMe = me.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
