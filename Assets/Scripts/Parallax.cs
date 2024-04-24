b7d3f8f03f38545b1944d442d108668c065cfbab
AndrewOL1 <aoleary7@student.fitchburgstate.edu>
2024-04-24 14:52:01 -0400

    Background

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -1,49 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Parallax : MonoBehaviour
 {
-    private float length, startpos;
-    public GameObject cam;
-    public float parallaxEffect;
-    float percent = 0.0f;
-    private bool positive;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Renderer image;
+    [SerializeField] private float speed;
+    private void Update()
     {
-        startpos = transform.position.x;
-        length = GetComponent<SpriteRenderer>().bounds.size.x;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        times();
-        //float dist = (cam.transform.position.x * parallaxEffect);//cam never moves so time
-        float dist = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
-    }
-    private void times()
-    {
-
-
-        if (percent == 0)
-        {
-            positive = true;
-        }
-        else if (percent == 1)
-        {
-            positive = false;
-        }
-        if (positive)
-        {
-            percent = percent + 0.001f;
-        }
-        else
-        {
-            percent = percent - 0.001f;
-        }
-
+        image.material.mainTextureOffset = new Vector2(speed * Time.time, 0);
     }
 }
